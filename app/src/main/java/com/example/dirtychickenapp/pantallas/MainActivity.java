75ab126d6fbebbcad71e7ed8d0e7e88c945c371f
adolfo <adolfocp1972@gmail.com>
2023-11-27 16:44:30 -0600

    login

--- app/src/main/java/com/example/dirtychickenapp/pantallas/MainActivity.java
+++ app/src/main/java/com/example/dirtychickenapp/pantallas/MainActivity.java
@@ -26,19 +26,20 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnRegistro;
-    Button btnIngreso;
+    //Button btnRegistro;
+    //Button btnIngreso;
     SQLiteConexion conexion;
     ArrayList<Cliente> listClientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnRegistro=(Button)findViewById(R.id.btnRegistro);
-        btnIngreso=(Button)findViewById(R.id.btnIngreso);
-        btnRegistro.setOnClickListener(e->permisos());
+        //btnRegistro=(Button)findViewById(R.id.btnRegistro);
+        //btnIngreso=(Button)findViewById(R.id.btnIngreso);
+        //btnRegistro.setOnClickListener(e->permisos());
         conexion = new SQLiteConexion(this, Transacciones.nameDB, null, 1);
-        getCliente();
+        permisos();
+
 
 
     }
@@ -62,28 +63,53 @@ public class MainActivity extends AppCompatActivity {
         }
 
         cursor.close();
-        if (listClientes != null && !listClientes.isEmpty()) {
-            btnRegistro.setEnabled(false);
-        } else {
-            btnRegistro.setEnabled(true);
+        if (listClientes.isEmpty()) {
+            registrarCliente();
+        }  else {
+            // Mostrar el nombre del cliente en el Toast
+            String nombreCliente = listClientes.get(0).getNombre(); // Suponiendo que solo quieres mostrar el primer cliente
+            String mensaje = getString(R.string.RespuestaTemp) + " " + nombreCliente;
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
         }
 
     }
 
+    private static final int MY_PERMISSIONS_REQUEST = 101;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSIONS_REQUEST) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                getCliente();
+            } else {
+
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private void permisos() {
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST);
         } else {
-            registrarCliente();
+            getCliente();
         }
     }
+
+
 
     private void registrarCliente() {
         Intent intent=new Intent(MainActivity.this, RegistrarCliente.class);
         startActivity(intent);
 
     }
+
+
 
 }
