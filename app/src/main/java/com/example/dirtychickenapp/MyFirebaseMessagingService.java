7cdabbc8eab5baf9d59f0f32a9700426d40253cc
montaniaRoja <adolfocp1972@gmail.com>
2023-12-07 19:38:14 -0600

    ultimo commit

--- app/src/main/java/com/example/dirtychickenapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/dirtychickenapp/MyFirebaseMessagingService.java
@@ -16,40 +16,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Verificar si el mensaje tiene datos
+
         if (remoteMessage.getData().size() > 0) {
-            // Manejar los datos del mensaje
+
             handleDataMessage(remoteMessage.getData());
         }
 
-        // Verificar si el mensaje tiene una notificación
+
         if (remoteMessage.getNotification() != null) {
-            // Manejar la notificación
+
             handleNotification(remoteMessage.getNotification());
         }
     }
 
     private void handleDataMessage(Map<String, String> data) {
-        // Aquí puedes procesar los datos del mensaje, por ejemplo, extraer información específica
-        // que el servidor haya incluido en el mensaje.
+
         String mensaje = data.get("mensaje");
         String otroDato = data.get("otro_dato");
 
-        // Puedes mostrar una notificación, actualizar la interfaz de usuario, etc., según tus necesidades.
+
     }
 
     private void handleNotification(RemoteMessage.Notification notification) {
-        // Aquí puedes manejar la parte visual de la notificación si es necesario.
+
         String title = notification.getTitle();
         String body = notification.getBody();
 
-        // Mostrar la notificación en la barra de notificaciones, por ejemplo
+
         showNotification(title, body);
     }
 
     private void showNotification(String title, String body) {
-        // Aquí puedes utilizar la clase NotificationManager para mostrar una notificación.
-        // Esto dependerá de cómo quieras diseñar y mostrar tus notificaciones.
+
 
         // Ejemplo básico:
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
